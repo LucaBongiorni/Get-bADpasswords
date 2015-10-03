ba7c9bec03223129fa59da7b6eb286ee9d89f9a2
ZilentJack <jhh@heidelbergit.dk>
2015-10-03 13:56:23 +0200

    Update Get-bADpasswords.ps1

--- Get-bADpasswords.ps1
+++ Get-bADpasswords.ps1
@@ -1,23 +1,21 @@
 <#
   .SYNOPSIS
-    Compare password hashes of active Active Directory users with list of bad or non-compliant passwords (e.g. hackers first guess in brute-force attack).
-    - Multiple wordlist can be used.
+    Compare password hashes of enabled Active Directory users with one or more lists of bad, weak or non-compliant passwords (e.g. hackers first guess during a brute-force attack).
+    - Multiple word lists can be used.
     - Can write log and CSV file output.
-    - Must be excuted with 'Domain Admin' or 'Domain Controller' permissions (or the like).
-
-    Requires latest PowerShell version from Windows Management Framework 5.0 (Production Preview)
+    - Must be excuted with 'Domain Admin' or 'Domain Controller' permissions (or equivalent).
     
     Requires PS Module "DSInternals" to be present on executing host. Please follow install instructions from there.
     - Found here: https://www.powershellgallery.com/packages/DSInternals/
     - More info:  https://www.dsinternals.com/en/
 
-    Note: this script does not modify input from wordlists, like switching to upper/lower case etc. Each word in wordlist is taken as-is. Use other tools to generate wordlists if needed.
+    Note: this script does not modify or permutate input from word lists, like switching to upper/lower case etc. Each word in word list is taken as-is. Use other tools to generate word lists if needed.
 	
   .DESCRIPTION
-    Compare password hashes of active Active Directory users with list of bad or non-compliant passwords.
+    Compare password hashes of active Active Directory users with list of bad, weak or non-compliant passwords.
 
   .PARAMETER arrBadPasswordFiles / Wordlists
-    Input paths for wordlists with bad passwords. Only "BadPasswords.txt" is used if nothing specified.
+    Input paths for word lists with weak passwords. Only "BadPasswords.txt" is used if nothing specified.
 
   .PARAMETER strDomainController / DC / DomainController
     Input name of the Domain Controller to query, e.g. "DC1".
@@ -26,7 +24,7 @@
     Input the AD Naming Context, e.g. "DC=AD,DC=HEIDELBERG,DC=NU".
 
   .PARAMETER bolWriteToCsvFile / WriteToCsvFile / CSV
-    Create dump CSV file of users with bad passwords.
+    Create dump CSV file of users with weak passwords.
 
   .PARAMETER bolWriteToLogFile / WriteToLogFile / Log
     Create log file with script execution status.
@@ -35,7 +33,7 @@
     Make the log file verbose - more detailed logging.
 
   .PARAMETER bolWriteClearTextPasswordsToLogFile / WriteClearTextPasswordsToLogFile / LogPasswords
-    Write clear text value of bad passwords to log file.
+    Write clear text value of detected weak passwords to log file.
 
   .EXAMPLE
     PS C:\> Get-bADpasswords -DC 'DC1' -NC 'DC=AD,DC=HEIDELBERG,DC=NU' -Log -LogVerbose -LogPasswords -CSV -Wordlists "BadPasswords.txt","Rockyou.txt" -Verbose
@@ -43,22 +41,22 @@
     1. Contact 'DC1' and as for users under Naming Context 'DC=AD,DC=HEIDELBERG,DC=NU'
     2. Write a log file (name is hardcoded in script and gets a timestamp for each execution).
     3. Make log file verbose - more detailed information.
-    4. Also log clear text (bad) passwords in the log file.
-    5. Dump CSV file of users with bad or non-compliant passwords. Use file for e.g. warning-email or force password change on next logon (other script/system).
-    6. Use two wordlists, "BadPasswords.txt" and "Rockyou.txt", from current directory.
+    4. Also log clear text (weak) passwords in the log file.
+    5. Dump CSV file of users with weak passwords. Use file for e.g. warning-email or force password change on next logon (other script/system).
+    6. Use two word lists, "BadPasswords.txt" and "Rockyou.txt", from current directory.
     7. Verbose logging to console.
 
   .EXAMPLE
     PS C:\> Get-bADpasswords -DC 'DC1' -NC 'DC=AD,DC=HEIDELBERG,DC=NU' -Wordlists "C:\Wordlists\OtherBadPasswords.txt" -Verbose
     
-    1. This will change the wordlist input to another file in another directory.
+    1. This will change the word list input to another file in another directory.
     2. Verbose logging to console.
 
   .EXAMPLE
     PS C:\> Get-bADpasswords -DC 'DC1' -NC 'DC=AD,DC=HEIDELBERG,DC=NU' -CSV
     
     1. This could be for running as a Scheduled Task e.g. on a DC as SYSTEM.
-    2. Will just dump CSV file of users with bad or non-compliant passwords. Use file for e.g. warning-email or force password change on next logon (other script/system).
+    2. Will just dump CSV file of users with weak passwords. Use file for e.g. warning-email or force password change on next logon (other script/system).
 
   .LINK
     Get latest version here: https://github.com/ZilentJack/Get-bADpasswords
@@ -81,9 +79,9 @@
      KI-0001: - none at this point -
 
     Change Requests (not prioritized, may or may not be implemented in future version):
-     CR-0001: Force end-users with bad passwords to change password on next logon.
-     CR-0002: Send e-mail to end-user with bad passwords and/or IT Security Manager.
-     CR-0003: Do permutations on input from wordlists, like add current year etc. (better tools for this out there).
+     CR-0001: Force end-users with weak passwords to change password on next logon.
+     CR-0002: Send e-mail to end-user with weak passwords and/or IT Security Manager.
+     CR-0003: Do permutations on input from word lists, like add current year etc. (better tools for this out there).
      CR-0004: Make log & CSV file naming (and timestamp) optional.
 
     Verbose output:
@@ -95,7 +93,7 @@ Function Get-bADpasswords
   [CmdletBinding()]
   param
   (
-    [Parameter(HelpMessage = 'Input paths for wordlists with bad passwords. Only "BadPasswords.txt" is used if nothing specified.')]
+    [Parameter(HelpMessage = 'Input paths for word lists with weak passwords. Only "BadPasswords.txt" is used if nothing specified.')]
     [alias("Wordlists")]
     [array]
     $arrBadPasswordFiles = @('BadPasswords.txt'),
@@ -112,7 +110,7 @@ Function Get-bADpasswords
     [string]
     $strNamingContext,
 
-    [Parameter(HelpMessage = 'Create dump CSV file of users with bad passwords.')]
+    [Parameter(HelpMessage = 'Create dump CSV file of users with weak passwords.')]
     [alias("CSV")]
     [switch]
     $bolWriteToCsvFile,
@@ -127,7 +125,7 @@ Function Get-bADpasswords
     [switch]
     $bolWriteVerboseInfoToLogfile,
 
-    [Parameter(HelpMessage = 'Write clear text value of bad passwords to log file.')]
+    [Parameter(HelpMessage = 'Write clear text value of weak passwords to log file.')]
     [alias("LogPasswords")]
     [switch]
     $bolWriteClearTextPasswordsToLogFile
@@ -207,7 +205,7 @@ Function Get-bADpasswords
     If ($bolWriteToLogFile -and $bolWriteVerboseInfoToLogfile) {LogWrite -Logfile $LogFileName -LogEntryString "| Logfile: $LogFileName" -LogEntryType INFO -TimeStamp}
     If ($bolWriteToLogFile -and $bolWriteVerboseInfoToLogfile) {LogWrite -Logfile $LogFileName -LogEntryString "| CSVfile: $CsvFileName" -LogEntryType INFO -TimeStamp}
 
-    # Create empty hash table for bad passwords
+    # Create empty hash table for bad/weak passwords
     $htBadPasswords = @{}
 
     # Populate array with usernames and NT hash values for enabled users only
@@ -244,19 +242,19 @@ Function Get-bADpasswords
     If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "AD returned $intUsersAndHashesFromAD usernames and NT hashes!" -LogEntryType DATA -TimeStamp}
     Write-Verbose "AD returned $intUsersAndHashesFromAD usernames and NT hashes!"
 
-    If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Loading bad password wordlists..." -LogEntryType INFO -TimeStamp}
-    Write-Verbose "Loading bad password wordlists..."
+    If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Loading bad password word lists..." -LogEntryType INFO -TimeStamp}
+    Write-Verbose "Loading bad password word lists..."
 
-    # Load each wordlist
+    # Load each word list
     Foreach ($WordlistPath in $arrBadPasswordFiles)
     {
-        If ($bolWriteToLogFile -and $bolWriteVerboseInfoToLogfile) {LogWrite -Logfile $LogFileName -LogEntryString "| Checking wordlist: $WordlistPath" -LogEntryType INFO -TimeStamp}
-        Write-Verbose "|Checking wordlist: $WordlistPath"
+        If ($bolWriteToLogFile -and $bolWriteVerboseInfoToLogfile) {LogWrite -Logfile $LogFileName -LogEntryString "| Checking word list: $WordlistPath" -LogEntryType INFO -TimeStamp}
+        Write-Verbose "|Checking word list: $WordlistPath"
 
         If (Test-Path $WordlistPath)
         {
-            If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Wordlist file found: $WordlistPath" -LogEntryType INFO -TimeStamp}
-            Write-Verbose "Wordlist file found: $WordlistPath"
+            If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Word list file found: $WordlistPath" -LogEntryType INFO -TimeStamp}
+            Write-Verbose "Word list file found: $WordlistPath"
         
             $BadPasswordList = Get-Content -Path $WordlistPath
 
@@ -279,30 +277,30 @@ Function Get-bADpasswords
             } # Foreach BadPassword
 
         }
-        Else # Wordlist not found
+        Else # Word list not found
         {
-            If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Logfile not found: $WordlistPath" -LogEntryType FAIL -TimeStamp}
-            Write-Verbose "Logfile not found: $WordlistPath"
+            If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Word list not found: $WordlistPath" -LogEntryType FAIL -TimeStamp}
+            Write-Verbose "Word list not found: $WordlistPath"
         }
 
     } # Foreach BadPassword file
 
     $intBadPasswordsInLists = $htBadPasswords.Count
 
-    # We can only continue, if we got bad passwords from wordlists
+    # We can only continue, if we got weak passwords from word lists
     If ($intBadPasswordsInLists -lt 1)
     {
-        If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "No data from wordlists - will quit!" -LogEntryType FAIL -TimeStamp}
-        Write-Verbose "No data from wordlists - will quit!"
+        If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "No data from word lists - will quit!" -LogEntryType FAIL -TimeStamp}
+        Write-Verbose "No data from word lists - will quit!"
         GetOut
     }
 
     # Let's deliver the good news
-    If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Wordlists had a total of $intBadPasswordsInLists unique bad passwords!" -LogEntryType DATA -TimeStamp}
-    Write-Verbose "Wordlists had a total of $intBadPasswordsInLists unique bad passwords!"
+    If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Word lists had a total of $intBadPasswordsInLists unique weak passwords!" -LogEntryType DATA -TimeStamp}
+    Write-Verbose "Word lists had a total of $intBadPasswordsInLists unique weak passwords!"
 
-    If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Wordlists had a total of $intBadPasswordsInListsDuplicates duplicate bad passwords!" -LogEntryType DATA -TimeStamp}
-    Write-Verbose "Wordlists had a total of $intBadPasswordsInListsDuplicates duplicate bad passwords!"
+    If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Word lists had a total of $intBadPasswordsInListsDuplicates duplicate weak passwords!" -LogEntryType DATA -TimeStamp}
+    Write-Verbose "Word lists had a total of $intBadPasswordsInListsDuplicates duplicate weak passwords!"
 
     Foreach ($objUser in $arrUsersAndHashes)
     {
@@ -318,10 +316,10 @@ Function Get-bADpasswords
             $strUserBadPasswordClearText = $htBadPasswords.Get_Item($strUserNTHashHex)
             If ($bolWriteToLogFile)
             {
-                If ($bolWriteClearTextPasswordsToLogFile){LogWrite -Logfile $LogFileName -LogEntryString "Bad password found for user: $strUserSamAccountName = $strUserBadPasswordClearText" -LogEntryType INFO -TimeStamp}
-                Else {LogWrite -Logfile $LogFileName -LogEntryString "Bad password found for user: $strUserSamAccountName" -LogEntryType INFO -TimeStamp}
+                If ($bolWriteClearTextPasswordsToLogFile){LogWrite -Logfile $LogFileName -LogEntryString "Weak password found for user: $strUserSamAccountName = $strUserBadPasswordClearText" -LogEntryType INFO -TimeStamp}
+                Else {LogWrite -Logfile $LogFileName -LogEntryString "Weak password found for user: $strUserSamAccountName" -LogEntryType INFO -TimeStamp}
             }
-            Write-Verbose "Bad password found for user: $strUserSamAccountName = $strUserBadPasswordClearText"
+            Write-Verbose "Weak password found for user: $strUserSamAccountName = $strUserBadPasswordClearText"
 
             # Handle CSV fil output
             If ($bolWriteToCsvFile)
@@ -336,9 +334,9 @@ Function Get-bADpasswords
         }
     }
 
-    # Give status on found bad passwords
-    If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Found $intBadPasswordsFound bad passwords!" -LogEntryType DATA -TimeStamp}
-    Write-Verbose "Found $intBadPasswordsFound bad passwords!"
+    # Give status on found weak passwords
+    If ($bolWriteToLogFile) {LogWrite -Logfile $LogFileName -LogEntryString "Found $intBadPasswordsFound weak passwords!" -LogEntryType DATA -TimeStamp}
+    Write-Verbose "Found $intBadPasswordsFound weak passwords!"
 
     # Let's exit gracefully
     GetOut
